2e16f6a8509d0c7defe3e49ac7266718c3816adc
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-25 13:27:19 +0300

    JSX - Javascript

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -13,28 +13,17 @@ const BookList = () => {
   )
 }
 
+const title = `Oh, the Places You'll Go!`
+const author = 'Dr. Seuss'
+
 const Book = () => {
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src='./images/book-1.jpg' alt="Oh, the Places You'll Go!" />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
-}
-
-const Image = () => (
-  <img src='./images/book-1.jpg' alt="Oh, the Places You'll Go!" />
-)
-const Title = () => <h2>Oh, the Places You'll Go!</h2>
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75',
-    marginTop: '0.5rem',
-  }
-
-  return <h4 style={inlineHeadingStyles}>Dr. Seuss</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
