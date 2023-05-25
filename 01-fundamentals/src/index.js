f37a9d4e31c565988220426917d4717e8f05e3a7
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-25 10:11:54 +0300

    BookList

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h2>John Doe</h2>
-const Message = () => {
-  return <p>this is my message</p>
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL600_SR600,400_.jpg"
+    alt="Oh, the Places You'll Go!"
+  />
+)
+const Title = () => <h2>Oh, the Places You'll Go!</h2>
+const Author = () => {
+  return <h4>Dr. Seuss</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Greeting />)
+root.render(<BookList />)
