627dbd0a8301f7329941383aa962caa312c4035b
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-25 22:55:16 +0300

    Prop Drilling

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -18,25 +18,26 @@ const books = [
 ]
 
 const BookList = () => {
+  const someValue = 'shakeAndBake'
+  const displayValue = () => {
+    console.log(someValue)
+  }
+
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book {...book} key={book.id} />
+        return <Book {...book} key={book.id} displayValue={displayValue} />
       })}
     </section>
   )
 }
 
-const Book = ({ img, title, author }) => {
-  const displayTitle = () => {
-    console.log(title)
-  }
-
+const Book = ({ img, title, author, displayValue }) => {
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>click me</button>
       <h4>{author}</h4>
     </article>
   )
