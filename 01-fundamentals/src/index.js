3b47abe2c742ccf6fea418c9cc2e80c352aed129
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-25 15:45:33 +0300

    Key Prop

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -4,11 +4,13 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     author: 'Dr. Seuss',
     title: `Oh, the Places You'll Go!`,
     img: './images/book-1.jpg',
   },
   {
+    id: 2,
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
@@ -19,8 +21,8 @@ const BookList = () => {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { img, title, author } = book
-        return <Book img={img} title={title} author={author} />
+        const { img, title, author, id } = book
+        return <Book key={id} img={img} title={title} author={author} />
       })}
     </section>
   )
