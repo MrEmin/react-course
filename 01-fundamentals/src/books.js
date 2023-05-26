b75ede304ee1779ba403787ab3ee02a6a55835e7
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-26 11:18:05 +0300

    ES6 Modules

---
+++ 01-fundamentals/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    id: 1,
+    author: 'Dr. Seuss',
+    title: `Oh, the Places You'll Go!`,
+    img: './images/book-1.jpg',
+  },
+  {
+    id: 2,
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+  },
+]
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,44 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const books = [
-  {
-    id: 1,
-    author: 'Dr. Seuss',
-    title: `Oh, the Places You'll Go!`,
-    img: './images/book-1.jpg',
-  },
-  {
-    id: 2,
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
-  },
-]
+import { books } from './books'
+import Book from './Book'
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id)
-    console.log(book)
-  }
-
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />
+        return <Book {...book} key={book.id} />
       })}
     </section>
-  )
-}
-
-const Book = ({ img, title, author, getBook, id }) => {
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>click me</button>
-      <h4>{author}</h4>
-    </article>
   )
 }
 
