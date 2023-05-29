c8e7ebfe1492dd49339a4d4c4f10a8a666350cc3
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-29 11:43:22 +0300

    UseState Fundamentals

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,18 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateBasics;
+const UseStateBasics = () => {
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <h4>You clicked {count} times</h4>
+      <button type='button' onClick={handleClick} className='btn'>
+        Increase
+      </button>
+    </div>
+  )
+}
+
+export default UseStateBasics
