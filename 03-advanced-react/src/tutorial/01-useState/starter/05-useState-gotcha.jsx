edd3dfbf0d62e45e4417ab909972ee0578399cfa
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-30 11:00:22 +0300

    UseState - SetTimeout Example

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -4,11 +4,12 @@ const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
 
   const handleClick = () => {
-    setValue((currentState) => {
-      const newState = currentState + 1
-      console.log(newState)
-      return newState
-    })
+    setTimeout(() => {
+      console.log('clicked the button')
+      setValue((currentValue) => {
+        return currentValue + 1
+      })
+    }, 3000)
   }
 
   return (
