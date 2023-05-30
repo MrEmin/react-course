4426b877bf5933645c544209b4d04791f8b06069
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-30 16:22:51 +0300

    UseEffect - Multiple Effects

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
-import Final from './tutorial/02-useEffect/final/01-code-example'
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
 
 function App() {
   return (
