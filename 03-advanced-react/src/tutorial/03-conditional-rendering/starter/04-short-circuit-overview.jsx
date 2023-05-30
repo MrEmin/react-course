cdc50d3f3006123d2537857d7a6544e86ac5ef8b
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-30 22:01:38 +0300

    Short Circuit - Overview

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
-};
-export default ShortCircuitOverview;
+  // falsy
+  const [text, setText] = useState('')
+
+  // truthy
+  const [name, setName] = useState('susan')
+
+  const codeExample = text || 'hello world'
+
+  return (
+    <div>
+      <h4>Falsy OR : {text || 'hello world'}</h4>
+      <h4>Falsy AND : {text && 'hello world'}</h4>
+      <h4>Truthy OR : {name || 'HELLO'}</h4>
+      <h4>Truthy AND : {name && 'HELLO'}</h4>
+      {codeExample}
+    </div>
+  )
+}
+export default ShortCircuitOverview
