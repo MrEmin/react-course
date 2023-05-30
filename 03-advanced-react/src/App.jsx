d7c46789651929eee882a89a6e4d6ba91537bbf5
MrEmin <emuhammet.ozdemir@gmail.com>
2023-05-31 01:21:01 +0300

    UseEffect Cleanup Function - Setup/Challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 import Final from './tutorial/03-conditional-rendering/final/04-short-circuit-overview'
 
 function App() {
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,22 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from 'react'
 
-export default CleanupFunction;
+const CleanupFunction = () => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <div>
+      <button className='btn' onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  )
+}
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('hmm, this is interesting')
+  }, [])
+  return <h1>hello there</h1>
+}
+
+export default CleanupFunction
