82d5979595f72be42a53061c6a7a1c038db0a8c4
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-10 22:49:05 +0300

    Folder Example

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,9 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
-import Final from './tutorial/03-conditional-rendering/final/04-short-circuit-overview'
+import Navbar from './tutorial/04-project-structure/starter/Navbar'
 
 function App() {
   return (
     <div className='container'>
-      <Starter />
-      {/* <Final /> */}
+      <Navbar />
     </div>
   )
 }
