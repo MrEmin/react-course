cb680972b16cc3483507fd82bf063a3f2d4d7369
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-11 11:29:46 +0300

    Leverage Javascript - Complete

--- 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ 03-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import avatar from '../../../assets/default-avatar.svg'
+
 export function Person({ name, nickName, images }) {
-  const img = images?.[0]?.small?.url
+  //const img = images && images[0] && images[0].small && images[0].small.url
+
+  // Optional Chaining and Or operator
+  // const img = images?.[0]?.small?.url || avatar
+
+  // Nullish coalescing operator
+  const img = images?.[0]?.small?.url ?? avatar
   return (
     <div>
       <section>
         <h4>{name}</h4>
         {nickName && <p>NickName: {nickName}</p>}
       </section>
-      {images && <img style={{ width: '150px' }} src={img} alt={name} />}
+      <img style={{ width: '150px' }} src={img} alt={name} />
     </div>
   )
 }
