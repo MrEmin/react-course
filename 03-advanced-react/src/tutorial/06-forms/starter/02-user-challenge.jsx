df9ea9e7bb215e9dc17d75ee8e534b7a0bfc1aa8
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-11 12:37:17 +0300

    User Challenge - Setup

--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,57 @@
+import { useState } from 'react'
+import { data } from '../../../data'
+
 const UserChallenge = () => {
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPeople([...people, { id: people.length + 1, name: name }])
+    setName('')
+  }
+
+  const removeItem = (id) => {
+    const newList = people.filter((person) => person.id !== id)
+    setPeople(newList)
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>
           submit
         </button>
       </form>
-      {/* render users below */}
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button
+              type='button'
+              className='btn'
+              onClick={() => removeItem(id)}
+            >
+              remove
+            </button>
+          </div>
+        )
+      })}
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
