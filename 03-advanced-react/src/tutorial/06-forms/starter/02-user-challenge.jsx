3b248bb1fb5d6909e4de860d1168a287133c0a45
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-11 12:59:59 +0300

    User Challenge - Add New User

--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
 import { data } from '../../../data'
 
-const UserChallenge = () => {
+const UsersChallenge = () => {
   const [name, setName] = useState('')
-  const [people, setPeople] = useState(data)
+  const [users, setUsers] = useState(data)
   const handleSubmit = (e) => {
     e.preventDefault()
-    setPeople([...people, { id: people.length + 1, name: name }])
+    if (!name) return
+    const fakeId = users.length + 1
+    const newUsers = { id: fakeId, name: name }
+    const updatedUsers = [...users, newUsers]
+    setUsers(updatedUsers)
     setName('')
   }
 
   const removeItem = (id) => {
-    const newList = people.filter((person) => person.id !== id)
-    setPeople(newList)
+    const newUsers = users.filter((person) => person.id !== id)
+    setUsers(newUsers)
   }
 
   return (
     <div>
       <form className='form' onSubmit={handleSubmit}>
-        <h4>Add User</h4>
+        <h4>Add Users</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
@@ -36,7 +40,8 @@ const UserChallenge = () => {
           submit
         </button>
       </form>
-      {people.map((person) => {
+      <h2>Userss</h2>
+      {users.map((person) => {
         const { id, name } = person
         return (
           <div key={id}>
@@ -54,4 +59,4 @@ const UserChallenge = () => {
     </div>
   )
 }
-export default UserChallenge
+export default UsersChallenge
