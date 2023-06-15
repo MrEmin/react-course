e9f006f04427947d2a88a401385f7ab1e911b3a6
MrEmin <emuhammet.ozdemir@gmail.com>
2023-06-15 23:26:30 +0300

    Reducer Basics

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -3,9 +3,15 @@ import { data } from '../../../data'
 
 const defaultState = {
   people: data,
+  isLoading: false,
 }
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR_LIST') {
+    console.log(state)
+    return { ...state, people: [] }
+  }
+}
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState)
@@ -16,13 +22,14 @@ const ReducerBasics = () => {
   }
 
   const clearList = () => {
+    dispatch({ type: 'CLEAR_LIST' })
     /* setPeople([]) */
   }
 
   const resetList = () => {
     /* setPeople(data) */
   }
-  console.log(state.people)
+  console.log(state)
   return (
     <div>
       {state.people.map((person) => {
