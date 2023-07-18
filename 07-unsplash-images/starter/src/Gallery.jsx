65fa4757bbe709eefa2fe6333d0bf8f1ed6baf1e
MrEmin <emuhammet.ozdemir@gmail.com>
2023-07-18 11:47:34 +0300

    Render Images

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -2,7 +2,6 @@ import { useQuery } from '@tanstack/react-query'
 import customFetch from './utils'
 import axios from 'axios'
 
-
 const url =
 
 const Gallery = () => {
@@ -13,7 +12,46 @@ const Gallery = () => {
       return result.data
     },
   })
-  console.log(response)
-  return <h2>Gallery</h2>
+
+  if (response.isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+
+  if (response.isError) {
+    return (
+      <section className='image-container'>
+        <h4>There was an error.</h4>
+      </section>
+    )
+  }
+
+  const results = response.data.results
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
+
+  return (
+    <section className='image-container'>
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className='img'
+          />
+        )
+      })}
+    </section>
+  )
 }
 export default Gallery
