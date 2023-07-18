472a35dff0c119f248372fa95ad4b455d9251f06
MrEmin <emuhammet.ozdemir@gmail.com>
2023-07-18 11:34:53 +0300

    Setup Query

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import customFetch from './utils'
+import axios from 'axios'
+
+
+const url =
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url)
+      return result.data
+    },
+  })
+  console.log(response)
   return <h2>Gallery</h2>
 }
 export default Gallery
