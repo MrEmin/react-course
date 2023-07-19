414ea9e68f4bbc1a9855b141e107de61fcf01e62
MrEmin <emuhammet.ozdemir@gmail.com>
2023-07-19 15:46:18 +0300

    Fetch Projects

--- 08-contentful-cms/src/Projects.jsx
+++ 08-contentful-cms/src/Projects.jsx
@@ -1,6 +1,8 @@
-import './fetchProjects'
+import { useFetchProjects } from './fetchProjects'
 
 const Projects = () => {
+  const { loading, projects } = useFetchProjects()
+  console.log(projects)
   return <h2>Projects</h2>
 }
 export default Projects
--- 08-contentful-cms/src/fetchProjects.jsx
+++ 08-contentful-cms/src/fetchProjects.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react'
 import { createClient } from 'contentful'
 
 const client = createClient({
-  space: '',
+  space: import.meta.env.VITE_SPACE,
   environment: 'master',
-  accessToken: '',
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 })
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response))
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' })
+      console.log(response)
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields
+        const id = item.sys.id
+        const img = image?.fields?.file?.url
+        return { title, url, id, img }
+      })
+      setProjects(projects)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return { loading, projects }
+}
