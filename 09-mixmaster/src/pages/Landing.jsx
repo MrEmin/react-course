65582b29bf195e8d7ba917b7e19b638369a0dea3
MrEmin <emuhammet.ozdemir@gmail.com>
2023-07-25 14:32:06 +0300

    SearchForm - Structure

--- 09-mixmaster/src/pages/Landing.jsx
+++ 09-mixmaster/src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom'
 import axios from 'axios'
 
 import CocktailList from '../components/CocktailList'
+import SearchForm from '../components/SearchForm'
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
@@ -16,6 +17,7 @@ const Landing = () => {
   const { drinks, searchTerm } = useLoaderData()
   return (
     <>
+      <SearchForm />
       <CocktailList drinks={drinks} />
     </>
   )
